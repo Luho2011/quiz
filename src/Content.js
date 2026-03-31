5f676fa289711acaebd8f22cda61f2f2ee40eb0a
Lukas <LukasHoltbrink@web.de>
2026-04-01 01:30:05 +0200

    Quiznamen

--- src/Content.js
+++ src/Content.js
@@ -23,40 +23,40 @@ function Content() {
     <div className='content'>
        <div className='game_buttons'>
         <Link to='/celeb' target="_blank">
-          <button className='game_button'><img className='questionmark_logo' src={questionmark} alt="" onClick={""}/></button>
+          <button className='game_button'><h2>Wer ist das?</h2><img className='questionmark_logo' src={questionmark} alt="" onClick={""}/></button>
         </Link>
         <Link to='/music' target="_blank">
-          <button className='game_button'><img className='rewind_logo' src={rewindlogo} alt="" onClick={""}/></button>
+          <button className='game_button'><h2>Musik back</h2><img className='rewind_logo' src={rewindlogo} alt="" onClick={""}/></button>
         </Link>
         <Link to='/belong' target="_blank">
-          <button className='game_button'><img className='geo_logo' src={belong} alt="" onClick={""}/></button>
+          <button className='game_button'><h2>Zuordnen</h2><img className='geo_logo' src={belong} alt="" onClick={""}/></button>
         </Link>
         <Link to='/zoom' target="_blank">
-          <button className='game_button'><img className='sort_logo' src={Sort} alt="" onClick={""}/></button>
+          <button className='game_button'><h2>Sorieren</h2><img className='sort_logo' src={Sort} alt="" onClick={""}/></button>
         </Link>
         <Link to='/history' target="_blank">
-          <button className='game_button'><img className='sort_logo' src={Globe} alt="" onClick={""}/></button>
+          <button className='game_button'><h2>Geographie</h2><img className='sort_logo' src={Globe} alt="" onClick={""}/></button>
         </Link>
         <Link to='/movies' target="_blank">
-          <button className='game_button'><img className='movie_logo' src={Movie} alt="" onClick={""}/></button>
+          <button className='game_button'><h2>Filmquiz 1</h2><img className='movie_logo' src={Movie} alt="" onClick={""}/></button>
         </Link>
         <Link to='/theme' target="_blank">
-          <button className='game_button'><img className='rewind_logo' src={theme} alt="" onClick={""}/></button>
+          <button className='game_button'><h2>Intros</h2><img className='rewind_logo' src={theme} alt="" onClick={""}/></button>
         </Link>
         <Link to='/movies2' target="_blank">
-          <button className='game_button'><img className='movie2_logo' src={Movie2} alt="" onClick={""}/></button>
+          <button className='game_button'><h2>Filmquiz 2</h2><img className='movie2_logo' src={Movie2} alt="" onClick={""}/></button>
         </Link>
         <Link to='/Claim' target="_blank">
-          <button className='game_button'><img className='claim_logo' src={Claim} alt="" onClick={""}/></button>
+          <button className='game_button'><h2>Wer lügt?</h2><img className='claim_logo' src={Claim} alt="" onClick={""}/></button>
         </Link>
         <Link to='/WhoKnowsMore' target="_blank">
-          <button className='game_button'><img className='whoknowsmore' src={WhoKnowsMore} alt="" onClick={""}/></button>
+          <button className='game_button'><h2>WhoKnowsMore</h2><img className='whoknowsmore' src={WhoKnowsMore} alt="" onClick={""}/></button>
         </Link>
         <Link to='/playlistSetupHitster' target="_blank">
-          <button className='game_button'><img className='musicTrivia' src={musicTrivia} alt="" onClick={""}/></button>
+          <button className='game_button'><h2>Hitster</h2><img className='musicTrivia' src={musicTrivia} alt="" onClick={""}/></button>
         </Link>
         <Link to='/association' target="_blank">
-          <button className='game_button'><img className='musicTrivia' src={WordsAssociation} alt="" onClick={""}/></button>
+          <button className='game_button'><h2>Assoziation</h2><img className='musicTrivia' src={WordsAssociation} alt="" onClick={""}/></button>
         </Link>
        </div>
     </div>
